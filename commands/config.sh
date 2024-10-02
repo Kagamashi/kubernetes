6995ed508a3acc168976c6be96ae233cf5f9cebf
Mikolaj Borecki <mikolaj.borecki@lhsystems.com>
2024-10-02 19:12:55 +0200

    Added new file: commands/config.sh

---
+++ commands/config.sh
@@ -0,0 +1,20 @@
+# kubectl config
+# Manage kubeconfig files that define how to connect to clusters, users, and contexts.
+
+# Usage:
+kubectl config [subcommand]
+
+# Example (set a context):
+kubectl config use-context my-cluster
+
+# Useful Subcommands:
+# - `kubectl config get-contexts`: List available contexts in your kubeconfig.
+# - `kubectl config use-context`: Switch between contexts.
+# - `kubectl config view`: Show details of your kubeconfig file.
+
+# Tips:
+# - Use `kubectl config use-context` to easily switch between different clusters or environments.
+# - `kubectl config set-context` allows you to modify or create new contexts in your kubeconfig file.
+
+# View context of aws user
+kubectl config view --kubeconfig=my-kube-config -o jsonpath="{.contexts[?(@.context.user=='aws-user')].name}" > /opt/outputs/aws-context-name
