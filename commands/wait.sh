3bc8cbdb73f6c6c760d97a47ff3a1492da76dada
Mikolaj Borecki <mikolaj.borecki@lhsystems.com>
2025-01-23 14:13:33 +0100

    Modified: commands/wait.sh

--- commands/wait.sh
+++ commands/wait.sh
@@ -1,16 +1,12 @@
-# kubectl wait
 # Wait for a specific condition to be met for a resource. This is useful for scripting, ensuring that resources reach a desired state before proceeding with other operations.
-
-# Usage:
 kubectl wait [resource-type]/[resource-name] --for=[condition]
 
-# Example (wait for a Pod to be ready):
+# wait for a Pod to be ready
 kubectl wait pod/my-pod --for=condition=ready --timeout=60s
 
-# Example (wait for a Deployment to be available):
+# wait for a Deployment to be available
 kubectl wait deployment/my-deployment --for=condition=available
 
-# Useful Flags:
 # --timeout: Specify a time limit for waiting (e.g., 30s, 1m).
 kubectl wait pod/my-pod --for=condition=ready --timeout=90s
 
