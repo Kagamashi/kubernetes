424e9b9c5dfb201d8dc2b7d3d8c402a3ea8f65ad
Mikolaj Borecki <mikolaj.borecki@lhsystems.com>
2025-01-23 13:48:59 +0100

    Modified: commands/attach.sh

--- commands/attach.sh
+++ commands/attach.sh
@@ -1,10 +1,11 @@
 # Attach to a running container in a Pod to view its output or interact with it.
 kubectl attach [pod-name] [-c [container-name]]
 
-# Example (attach to a Pod):
+
+# attach to a Pod
 kubectl attach my-pod
 
-# Example (attach to a specific container in a multi-container Pod):
+# attach to a specific container in a multi-container Pod
 kubectl attach my-pod -c my-container
 
 # --stdin/-i: Pass stdin to the container, allowing interactive commands.
