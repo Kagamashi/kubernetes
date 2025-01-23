1e644ca4c675899ffcf6a61e049adc2b0bb25e23
Mikolaj Borecki <mikolaj.borecki@lhsystems.com>
2025-01-23 14:04:05 +0100

    Deleted: commands/annotate.sh

--- commands/annotate.sh
+++
@@ -1,9 +0,0 @@
-# Adds or updates the annotations on a resource.
-kubectl annotate [resource-type] [resource-name] [annotation-key]=[value]
-
-
-# --overwrite:  Allows overwriting an existing annotation.
-kubectl annotate pod my-pod description="Updated pod" --overwrite
-
-# Tips:
-# - Use annotations to store metadata that can be used by tools or automation systems.
