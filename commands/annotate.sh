63700422b0cb97d5092d5dbfea6f4e139f142fec
Mikolaj Borecki <mikolaj.borecki@lhsystems.com>
2025-01-23 13:48:59 +0100

    Modified: commands/annotate.sh

--- commands/annotate.sh
+++ commands/annotate.sh
@@ -1,6 +1,7 @@
 # Adds or updates the annotations on a resource.
 kubectl annotate [resource-type] [resource-name] [annotation-key]=[value]
 
+
 # --overwrite:  Allows overwriting an existing annotation.
 kubectl annotate pod my-pod description="Updated pod" --overwrite
 
